8dcdac537e6c35c9497df6394747debd5a31a5ab
huqian301 <huqian301@gmail.com>
2020-08-19 18:40:13 -0700

    basic code for all

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    const apiKey = 'e6739b112bc9427a98862a95ca09ef9c';
+    const baseUrl = `https://api.spoonacular.com/recipes/716429/information?apiKey=${apiKey}&includeNutrition=true`;
+
+    $(document).on('click', function (event) {
+        event.preventDefault();
+    });
+
+    $.ajax({
+        url: `${baseUrl}`,
+        method: 'GET',
+    }).then(function (response) {
+        console.log(response);
+    })
+
+})
+
