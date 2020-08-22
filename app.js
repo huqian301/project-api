136cc4b1c0a781ba5d23f127c6d90618ce075514
huqian301 <huqian301@gmail.com>
2020-08-21 18:45:13 -0700

    testing api

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
+
+    const id =715538
     const apiKey = 'e6739b112bc9427a98862a95ca09ef9c';
-    const baseUrl = `https://api.spoonacular.com/recipes/716429/information?apiKey=${apiKey}&includeNutrition=true`;
+    // const baseUrl = `https://api.spoonacular.com/recipes/716429/information?apiKey=${apiKey}&includeNutrition=true`;
+    const baseUrl = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`;
 
     $(document).on('click', function (event) {
         event.preventDefault();
