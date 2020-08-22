dc7793fdd6983bb883e54ad101c2beffbaa58ad9
mmaciasjr <65560980+mmaciasjr@users.noreply.github.com>
2020-08-22 10:39:59 -0700

    Recipe HTML page

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 $(document).ready(function () {
-    const apiKey = 'e6739b112bc9427a98862a95ca09ef9c';
-    const baseUrl = `https://api.spoonacular.com/recipes/716429/information?apiKey=${apiKey}&includeNutrition=true`;
+  const apiKey = 'e6739b112bc9427a98862a95ca09ef9c';
+  const baseUrl = `https://api.spoonacular.com/recipes/716429/information?apiKey=${apiKey}&includeNutrition=true`;
 
-    $(document).on('click', function (event) {
-        event.preventDefault();
-    });
+  $(document).on('click', function (event) {
+    event.preventDefault();
+  });
+  //open full screen box
+  function openSearch() {
+    document.getElementById('myOverlay').style.display = 'block';
+  }
 
-    $.ajax({
-        url: `${baseUrl}`,
-        method: 'GET',
-    }).then(function (response) {
-        console.log(response);
-    })
-
-})
-
+  // Close the full screen search box
+  function closeSearch() {
+    document.getElementById('myOverlay').style.display = 'none';
+  }
+  $.ajax({
+    url: `${baseUrl}`,
+    method: 'GET',
+  }).then(function (response) {
+    console.log(response);
+  });
+});
