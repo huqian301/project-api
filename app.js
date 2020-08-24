b34f107904110e910a212883920d970902d25904
huqian301 <huqian301@gmail.com>
2020-08-23 18:41:21 -0700

    fixed the cards row repeating

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
 
     const recipeSearchInput = $('#recepieSearch');
-    const apiKey = 'e6739b112bc9427a98862a95ca09ef9c';
+    // const apiKey = 'e6739b112bc9427a98862a95ca09ef9c';
+    const apiKey = '37d6c566dffd4e65af7bad5bf87e9617';
     // const baseUrl = `https://api.spoonacular.com/recipes/716429/information?apiKey=${apiKey}&includeNutrition=true`;
     const baseUrl = `https://api.spoonacular.com/recipes/`;
 
@@ -17,9 +18,11 @@ $(document).ready(function () {
             method: 'GET',
         }).then(function (response) {
             console.log(response);
+
+            const $newDiv = $('<div>').addClass('row row-cols-1 row-cols-md-2');
+
             for (i = 0; i < 8; i++) {
                 id = response.results[i].id;
-
 
                 $.ajax({
                     url: `${baseUrl}${id}/information?apiKey=${apiKey}`,
@@ -27,7 +30,13 @@ $(document).ready(function () {
                 }).then(function (res) {
                     console.log(res);
 
-                    const $newDiv = $('<div>').addClass('row row-cols-1 row-cols-md-2');
+                    // const para = res.instructions;
+                    // const endWith = para.endsWith(".");
+
+                    console.log(para);
+                    
+
+                    // const $newDiv = $('<div>').addClass('row row-cols-1 row-cols-md-2');
                     const $newDiv1 = $('<div>').addClass('col mb-4');
                     const $newDiv2 = $('<div>').addClass('card');
                     const $newDiv3 = $('<div>').addClass('view overlay');
@@ -35,7 +44,7 @@ $(document).ready(function () {
                     const $newDiv4 = $('<div>').addClass('card-body');
                     const $h4 = $('<h4>').addClass('card-title').text(res.title);
                     const $hr = $('<hr>');
-                    const $p = $('<p>').addClass('card-text').text('Instruction:' +res.instructions);
+                    const $p = $('<p>').addClass('card-text').text('Instruction: ' +res.instructions);
                     const $button = $('<button>').addClass('btn btn-light-blue btn-md');
                     const $a = $('<a>').attr({href: res.sourceUrl, target :'_blank'}).text('Read more');
 
@@ -45,7 +54,7 @@ $(document).ready(function () {
 
                     $newDiv2.append($newDiv3,$newDiv4);
                     $newDiv1.append($newDiv2);
-                    $newDiv.append($newDiv1)
+                    $newDiv.append($newDiv1);
 
 
                     $('.container').append($newDiv);
